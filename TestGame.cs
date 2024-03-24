2fce3ca99be9fa15b4de2cf2e4aed68cba19e9e9
Serge Roy <serge.roy@gmail.com>
2024-03-24 10:25:17 -0300

    first commit

---
+++ TestGame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using var game = new TestGame();
+game.Run();
+
+public class TestGame : Game
+{
+    private readonly GraphicsDeviceManager _graphics;
+    private readonly Random _rnd = new();
+    private Color _backColor = Color.CornflowerBlue;
+    private bool _leftMouse;
+
+    public TestGame()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        IsMouseVisible = true;
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(_backColor);
+        base.Draw(gameTime);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+        
+        var leftMouse = Mouse.GetState().LeftButton == ButtonState.Pressed;
+
+        if (_leftMouse != leftMouse)
+        {
+            _backColor = new Color(_rnd.NextSingle(), _rnd.NextSingle(), _rnd.NextSingle());
+            _leftMouse = leftMouse;
+        }
+    }
+}
